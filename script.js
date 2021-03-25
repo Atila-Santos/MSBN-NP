7d057bf3d8bd945adff57b305c034b560c872d89
ATILA ELLVANGGER SANTOS <atilaecamila2@gmail.com>
2021-03-25 13:50:23 -0300

    :package: :up: função alerta

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
         document.querySelector(".container").classList.toggle("show-menu")
 );
+
+function clicou() {
+        alert("Obrigado por confirmar. Ahh... não esqueça a máscara");
+}
